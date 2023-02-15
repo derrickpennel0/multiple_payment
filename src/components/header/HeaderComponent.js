8686e64b8b591f691e7b127419776efeddb9ed09
TorsuD <torsuderrick2004@gmail.com>
2023-02-15 12:53:26 +0500

    header modifications

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { FiFeather } from "react-icons/fi";
 
 function HeaderComponent({ icon, title }) {
   return (
@@ -10,7 +9,7 @@ function HeaderComponent({ icon, title }) {
           alignItems: "center",
           padding: "5px",
           color: "white",
-          backgroundColor: "#37c6db",
+          backgroundColor: "rgb(21 163 183)",
           fontSize: "20px",
         }}
       >
--- src/screens/CashDeposit.js
+++ src/screens/CashDeposit.js
@@ -1,11 +1,12 @@
 import React from "react";
 import HeaderComponent from "../components/header/HeaderComponent";
+import { GiReceiveMoney } from '../../node_modules/react-icons/gi/index.esm';
 
 function CashDeposit() {
   return (
     <div>
       <div className="cash__deposit">
-        <HeaderComponent />
+        <HeaderComponent title="Cash Deposit" icon={<GiReceiveMoney />}/>
       </div>
     </div>
   );
